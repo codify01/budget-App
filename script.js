2d3f0164c5f306d6517aca5aa9ba14e1f5a6c1b8
Uthman Oluwaseun <amoleuthman@gmail.com>
2024-04-02 07:05:01 +0100

    init commit

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// let totalIncome = document.getElementById("totalIncome").innerHTML
+// let totalSpending = document.getElementById("totalspending").innerHTML
+
+
 const addIncome = ()=> {
-    alert("working")
+    let incomeValue = Number(prompt("Enter your monthly income:"))
+    console.log(incomeValue);
+    document.getElementById("totalIncome").innerHTML = "$" + incomeValue
 }
+
+const addSpending = ()=> {
+console.log(totalIncome, totalSpending);
+let incomeValue = Number(prompt("Enter how much your spending:"))
+    if (document.getElementById("totalIncome").innerHTML  != ""){
+        alert("you are good to go")
+        console.log(incomeValue);
+        document.getElementById("totalSpending").innerHTML = "$" + incomeValue
+    } else{
+        alert( "You don't have enough money!")
+    }
+
+}
+
